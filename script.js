c148b408820f7c73353138622596329b45d6656b
Bsargent23 <164093282+Bsargent23@users.noreply.github.com>
2024-04-30 22:09:54 +0000

    added name button and save function

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ button.addEventListener("click", function() {
   alert("Hello, World!");
 })
 
+// create a function to save the madlib
+function saveMadLib() {
+  console.log("saveMadLib() called");
+  var storyData = createMadLib()
+  db.collection("madlibs").doc(storyData.storyName).set(storyData); alert(storyData.storyName + " saved to database!");
+}
