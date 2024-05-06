396e20d371be0a628b7cd238876aee4e647f6152
Bsargent23 <164093282+Bsargent23@users.noreply.github.com>
2024-05-06 04:14:01 +0000

    update save function

--- script.js
+++ script.js
@@ -13,22 +13,53 @@ const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 console.log("firebase setup complete!");
 
+// create a function to show the answers
 
+function showAnswers() {
 
+  console.log("Answers() called");
+  
+  var Yourname =                 document.getElementById("Yourname").value;
 
+  var grade =
+    document.getElementById("grade").value;
 
-// create a function to save the madlib
-function saveMadLib() {
-  console.log("saveMadLib() called");
-  var storyData = createMadLib()
-    db.collection("madlibs").doc(storyData.storyName).set(storyData); alert(storyData.storyName + " saved to database!");
+  var learn =
+    document.getElementById("learn").value;
+
+    document.getElementById("StudentAnswers").innerHTML = "Your name is: " + Yourname + "</u>, . Grade: <u>" + grade + "</u>." + " You want to learn about: <u>" + learn + "</u>.";
+
+  var StudentAnswers= document.getElementById("StudentAnswers").innerHTML;
+  console.log("StudentAnswers: " + StudentAnswers);
+  
+    var storyData = {
+      timestamp: Date.now(),
+      StudentAnswers: StudentAnswers,
+      learn: learn,
+    grade: grade,
+    Yourname: Yourname
+
+    };
+
+    console.log("storyData: " + storyData);
+
+    var storyJSON = JSON.stringify(storyData);
+    console.log("storyJSON: " + storyJSON);
+    return storyData;
+  }
+
+// create a function to save the story
+function saveAnswer() {
+  console.log("saveAnswer() called");
+  var storyData = saveAnswer()
+    db.collection("Answers").doc(storyData.storyName).set(storyData); alert(storyData.storyName + " saved to database!");
 }
 
 
 // make sure that data is going to firestore
-collectionName = "test runs";
-docName = "test run 2";
-data = { myName: "Dan Stille", class: "Web Development" };
-db.collection(collectionName).doc(docName).set(data);
-console.log("data: " + data);
-alert(docName + " saved to database!!");
+//collectionName = "test runs";
+//docName = "test run 2";
+//data = { myName: "Dan Stille", class: "Web Development" };
+//db.collection(collectionName).doc(docName).set(data);
+//console.log("data: " + data);
+//alert(docName + " saved to database!!");
